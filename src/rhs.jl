c306f4fa6285906e29f9ed5e81016926e0459595
milankl <milankloewer@gmx.de>
2019-07-18 15:19:58 +0100

    inbounds speed-up

--- src/rhs.jl
+++ src/rhs.jl
@@ -6,8 +6,8 @@
 # end
 
 function rhs!(dxyz::Array{T,1},x::T,y::T,z::T,ρ::T,β::T,s_inv::T) where {T<:AbstractFloat}
-    dxyz[1] = y-x
-    dxyz[2] = x*(ρ-z*s_inv) - y
-    dxyz[3] = x*(y*s_inv) - β*z
+    @inbounds dxyz[1] = y-x
+    @inbounds dxyz[2] = x*(ρ-z*s_inv) - y
+    @inbounds dxyz[3] = x*(y*s_inv) - β*z
     return dxyz
 end
