67ed128cdf21cbc6eeba3513152234dc86bb1926
milankl <milankloewer@gmx.de>
2019-07-23 14:37:30 +0100

    simd, and clean up

--- src/rhs.jl
+++ src/rhs.jl
@@ -1,10 +1,3 @@
-# function rhs!(dxyz::Array{T,1},x::T,y::T,z::T,ρ::T,β::T,s::T) where {T<:AbstractFloat}
-#     dxyz[1] = y-x
-#     dxyz[2] = x*(ρ-z/s) - y
-#     dxyz[3] = x*(y/s) - β*z
-#     return dxyz
-# end
-
 function rhs!(dxyz::Array{T,1},x::T,y::T,z::T,ρ::T,β::T,s_inv::T) where {T<:AbstractFloat}
     @inbounds dxyz[1] = y-x
     @inbounds dxyz[2] = x*(ρ-z*s_inv) - y
