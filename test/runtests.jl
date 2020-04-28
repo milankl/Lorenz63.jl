b1a07caa92f299b0445f3b0f4dca5eac31221204
milankl <milankloewer@gmx.de>
2020-04-28 15:53:17 +0100

    No cirrus CI, scaling tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,46 @@
 using Lorenz63
 using Test
+using Statistics
 
 @testset "Equilibrium" begin
     XYZ = L63(xyz=[0.,0.,0.])
     @test all(XYZ[:,end] == [0.,0.,0.,])
+end
+
+@testset "Scaling up" begin
+    XYZ1 = L63(s=1.0)
+    XYZ10 = L63(s=10.0)
+
+    # simulations are actually different
+    @test XYZ1[:,end] != XYZ10[:,end]
+
+    # means are approximately same
+    @test isapprox(mean(XYZ1[1,:]),mean(XYZ10[1,:]),rtol=1e-6)
+    @test isapprox(mean(XYZ1[2,:]),mean(XYZ10[2,:]),rtol=1e-6)
+    @test isapprox(mean(XYZ1[3,:]),mean(XYZ10[3,:]),rtol=1e-6)
+
+    # variances are approximately same
+    @test isapprox(var(XYZ1[1,:]),var(XYZ10[1,:]),rtol=1e-6)
+    @test isapprox(var(XYZ1[2,:]),var(XYZ10[2,:]),rtol=1e-6)
+    @test isapprox(var(XYZ1[3,:]),var(XYZ10[3,:]),rtol=1e-6)
+end
+
+@testset "Scaling down" begin
+    XYZ1 = L63(s=1.0)
+    XYZ10 = L63(s=1/10)
+
+    # simulations are actually different
+    @test XYZ1[:,end] != XYZ10[:,end]
+
+    # means are approximately same
+    @test isapprox(mean(XYZ1[1,:]),mean(XYZ10[1,:]),rtol=1e-6)
+    @test isapprox(mean(XYZ1[2,:]),mean(XYZ10[2,:]),rtol=1e-6)
+    @test isapprox(mean(XYZ1[3,:]),mean(XYZ10[3,:]),rtol=1e-6)
+
+    # variances are approximately same
+    @test isapprox(var(XYZ1[1,:]),var(XYZ10[1,:]),rtol=1e-6)
+    @test isapprox(var(XYZ1[2,:]),var(XYZ10[2,:]),rtol=1e-6)
+    @test isapprox(var(XYZ1[3,:]),var(XYZ10[3,:]),rtol=1e-6)
 end
 
 @testset "Bounded" begin
